44060d27a908e88858799b5fff8775c0a914e9e0
Ryo Utsunomiya <utsunomiya@asial.co.jp>
2016-11-01 17:16:21 +0900

    Add 1.1

---
+++ chapter_01/1.1.c
@@ -0,0 +1,18 @@
+/**
+ * Exercise 1.1
+ *
+ * Run the “hello, world” program on your system.
+ * Experiment with leaving out parts of the program, to see what error messages you get.
+ */
+
+/**
+ * Note: Run following commands to execute this program.
+ * (1) gcc 1.1.c -o hello
+ * (2) ./hello
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+}
