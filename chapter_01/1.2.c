b480c3bbc9a8110d642bccf41ca90f0af946123f
Ryo Utsunomiya <utsunomiya@asial.co.jp>
2016-11-01 17:23:37 +0900

    Add 1.2

---
+++ chapter_01/1.2.c
@@ -0,0 +1,17 @@
+/**
+ * Exercise 1.2
+ *
+ * Experiment to find out what happens when printf’s argument string contains \c,
+ * where c is some character not listed above.
+ */
+
+/**
+ * When you compile this program, you might get the "unknown escape sequence" warning.
+ * When you run compiled executable, console will show "c", without backslash.
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("\c");
+}
