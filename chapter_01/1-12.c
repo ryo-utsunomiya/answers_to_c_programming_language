0ec85053829644d3a0b83a48c7f561ecb32606b3
Ryo Utsunomiya <millionsnowliving@gmail.com>
2016-12-18 15:53:34 +0900

    Add 1-12

---
+++ chapter_01/1-12.c
@@ -0,0 +1,21 @@
+/**
+ * Exercise 1-12
+ *
+ * Write a program that prints its input one word per line.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int c;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            putchar('\n');
+        } else {
+            putchar(c);
+        }
+    }
+
+    return 0;
+}
