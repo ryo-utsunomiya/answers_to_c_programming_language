ef83bab8a4dbf1e9ef122d09883831ab7dc7935c
Ryo Utsunomiya <millionsnowliving@gmail.com>
2016-12-18 16:51:30 +0900

    Add 1-15

---
+++ chapter_01/1-15.c
@@ -0,0 +1,29 @@
+/**
+ * Exercise 1-15
+ *
+ * Rewrite the temperature conversion program of Section 1.2 to use a function for conversion.
+ */
+
+#include <stdio.h>
+
+float fahrToCelsius(float fahr) {
+    return (5.0 / 9.0) * (fahr - 32.0);
+}
+
+int main(void) {
+    float fahr;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+
+    while (fahr <= upper) {
+        printf("%3.0f %6.1f\n", fahr, fahrToCelsius(fahr));
+        fahr += step;
+    }
+
+    return 0;
+}
