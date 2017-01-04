b44a1990f2c91f2d7e1bb2362b370b3cc25cb23f
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-01-04 23:28:13 +0900

    Add 1-24

---
+++ chapter_01/1-24.c
@@ -0,0 +1,14 @@
+/**
+ * Exercise 1-24
+ *
+ * Write a program to check a C program for rudimentary syntax errors like unbalanced parentheses, brackets and braces.
+ * Don’t forget about quotes, both single and double, escape sequences, and comments.
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Not implemented yet!");
+
+    return 0;
+}
